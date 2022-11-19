4d297002cd66539ca658025335c25399053c2add
nuintun <nuintun@qq.com>
2022-11-19 23:17:40 +0800

    feat: Update rect utils

--- src/utils/rect.ts
+++ src/utils/rect.ts
@@ -4,6 +4,14 @@
 
 import { Rect } from './interface';
 
+// 垂直模式映射表
+const VERTICAL_MODE: Record<string, boolean> = {
+  tb: true,
+  'tb-rl': true,
+  'vertical-rl': true,
+  'vertical-lr': true
+};
+
 /**
  * @function getBoundingRect
  * @function 标准化元素尺寸
@@ -11,14 +19,9 @@ import { Rect } from './interface';
  * @param contentBox  是否使用 contentBox 模式
  */
 export function getBoundingRect(entry: ResizeObserverEntry, contentBox?: boolean): Rect {
-  const mapping: Record<string, boolean> = {
-    tb: true,
-    'tb-rl': true,
-    'vertical-rl': true,
-    'vertical-lr': true
-  };
-  const { writingMode } = getComputedStyle(entry.target);
+  const vertical = VERTICAL_MODE[getComputedStyle(entry.target).writingMode];
   const [{ blockSize, inlineSize }] = contentBox ? entry.contentBoxSize : entry.borderBoxSize;
+  const viewport = vertical ? { width: blockSize, height: inlineSize } : { width: inlineSize, height: blockSize };
 
-  return mapping[writingMode] ? { width: blockSize, height: inlineSize } : { width: inlineSize, height: blockSize };
+  return __DEV__ ? Object.freeze(viewport) : viewport;
 }
